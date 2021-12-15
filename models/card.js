8a63d3dc2f9e6dff5100a4100de28fe84f0108fa
YD <yurovdigital@gmail.com>
2021-12-15 11:58:19 +0300

    fix after Review #1

--- models/card.js
+++ models/card.js
@@ -25,7 +25,7 @@ const cardSchema = new mongoose.Schema({
     required: true,
   },
   likes: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: [mongoose.Schema.Types.ObjectId],
     default: [],
   },
   createdAt: {
