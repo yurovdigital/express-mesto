a6a6a6c34ec90286f24ab1005a67884c61278a34
YD <yurovdigital@gmail.com>
2021-12-17 20:55:17 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -115,13 +115,13 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign(
+      const jwt = jwt.sign(
         { _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' }
       )
 
-      res.status(200).send({ token })
+      res.status(200).send({ jwt })
     })
     .catch(next)
 }
