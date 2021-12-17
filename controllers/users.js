4f4961a651a639b41786e98f5924a102f5afbdef
YD <yurovdigital@gmail.com>
2021-12-17 20:36:23 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const { NODE_ENV, JWT_SECRET } = process.env
 
